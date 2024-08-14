0993182ed02549c515392c37a15d3e7c596e585d
flam1ng <carlosamg87@gmail.com>
2024-08-14 17:59:39 +0200

    feat: app radio button component

---
+++ src/components/AppRadioButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <label v-for="option in options" :key="option.value">
+      <input
+        type="radio"
+        :value="option.value"
+        :checked="selectedCountry === option.value"
+        @change="handleChange(option.value)"
+      />
+      {{ option.label }}
+    </label>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AppRadioButton',
+    props: {
+      options: {
+        type: Array,
+        required: true,
+      },
+      selectedCountry: {
+        type: String,
+        required: true,
+      },
+    },
+    methods: {
+      handleChange(value) {
+        this.$emit('update-country', { key: 'targetCountries', value })
+      },
+    },
+  }
+</script>
